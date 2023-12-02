597d1bac86b82589d848e6810f3c1bc48e2d6e9d
Onni <onni.vitikainen@tuni.fi>
2023-12-02 23:17:34 +0200

    web version v.10

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,24 +1,12 @@
 const mysql = require('mysql2');
 
-
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.ROOT,
   database: process.env.SQLDB,
   password: process.env.SQLPASS,
+  port: process.env.SQLPORT
 });
 
-const checkConnection = async () => {
-  const promisePool = pool.promise();
 
-  try {
-    await promisePool.ping();
-    console.log('MySQL connection is active.');
-  } catch (error) {
-    console.error('Error pinging MySQL server:', error);
-  } finally {
-    promisePool.end(); 
-  }
-};
-
-module.exports = { pool, checkConnection };
+module.exports = connection.promise();
