aa7b00162312c1d47956824781b150ec1066b648
Onni <onni.vitikainen@tuni.fi>
2023-12-02 21:37:24 +0200

    DB execute

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,11 +8,11 @@ module.exports = class User {
   }
 
   static find(email) {
-    return db.promise().query('SELECT * FROM users WHERE email = ?', [email]);
+    return db.execute('SELECT * FROM users WHERE email = ?', [email]);
   }
 
   static save(user) {
-    return db.promise().query(
+    return db.execute(
       'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
       [user.name, user.email, user.password]
     );
